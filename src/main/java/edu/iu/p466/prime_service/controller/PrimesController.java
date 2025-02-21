704ce43ad7fbd0c3aa607591d19ab1c8116f33f8
arsnck <cnr2291@gmail.com>
2025-02-20 21:45:25 -0500

    update workflows

--- src/main/java/edu/iu/p466/prime_service/controller/PrimesController.java
+++ src/main/java/edu/iu/p466/prime_service/controller/PrimesController.java
@@ -1,4 +1,19 @@
 package edu.iu.p466.prime_service.controller;
 
+import edu.iu.p466.prime_service.service.IPrimeService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
+
 public class PrimesController {
+    IPrimeService PS;
+    public PrimesController(IPrimeService PS){
+        this.PS = PS;
+    }
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable int n){
+        return PS.isPrime(n);
+    }
 }
